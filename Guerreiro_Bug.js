6995397c4bf6911be3ca8d5615d27030aa608fe4
Ana <152903670+Analimait@users.noreply.github.com>
2024-01-01 21:31:35 -0300

    Add files via upload

---
+++ Guerreiro_Bug.js
@@ -0,0 +1,14 @@
+
+let heroi = {
+	nome: "Bug",
+    idade: 2,
+    tipo: "guerreiro",
+    ataque: "espada",
+};
+
+
+if (heroi.tipo = "guerreiro") {
+
+console.log(heroi.tipo + " atacou usando " + heroi.ataque)
+
+}
